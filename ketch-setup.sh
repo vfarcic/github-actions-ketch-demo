9c1396996cff43d9272f3b2cb6122fa252df87aa
Viktor Farcic <viktor@farcic.com>
2021-04-26 22:23:41 +0200

    Initial commit

--- ketch-setup.sh
+++ ketch-setup.sh
@@ -1,3 +1,5 @@
+curl -s https://raw.githubusercontent.com/shipa-corp/ketch/main/install.sh | bash
+
 kubectl apply --filename https://github.com/jetstack/cert-manager/releases/download/v1.0.3/cert-manager.yaml --validate=false
 
 kubectl --namespace cert-manager rollout status deployment cert-manager
